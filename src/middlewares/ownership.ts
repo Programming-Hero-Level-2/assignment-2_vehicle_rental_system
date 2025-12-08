7f83b2fd2757896251567b3c047ce2d487e87e1a
naiem_sl <naiemulislam.dev@gmail.com>
2025-12-08 13:02:57 +0600

    implemented ownership middleware

--- src/middlewares/ownership.ts
+++ src/middlewares/ownership.ts
@@ -3,13 +3,13 @@ import { ApiError } from '../utils/ApiError';
 
 const ownership = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const resourceOwnerId = req.params.userId; // Assuming the resource owner ID is passed as a URL parameter
+    const ownerId = req.params.userId;
 
     if (!req.user) {
       return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    if (req.user.id === resourceOwnerId || req.user.role === 'admin') {
+    if (req.user.id === ownerId || req.user.role === 'admin') {
       return next();
     }
 
--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -2,21 +2,24 @@ import express from 'express';
 import { userController } from './user.controller';
 import authenticated from '../../middlewares/authenticated';
 import authorize from '../../middlewares/authorize';
+import ownership from '../../middlewares/ownership';
 
 const router = express.Router();
 
 router.get('/', authenticated, authorize(['admin']), userController.getUsers);
-router.put(
-  '/:userId',
-  authenticated,
-  authorize(['customer']),
-  userController.updateUser
-);
-router.delete(
-  '/:userId',
-  authenticated,
-  authorize(['admin']),
-  userController.deleteUser
-);
+router
+  .put(
+    '/:userId',
+    authenticated,
+    ownership,
+    authorize(['admin', 'customer']),
+    userController.updateUser
+  )
+  .delete(
+    '/:userId',
+    authenticated,
+    authorize(['admin']),
+    userController.deleteUser
+  );
 
 export const userRouter = router;
